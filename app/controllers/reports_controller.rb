8176b637af88b145c4b9a3891a02123a892df3f6
botiuk <svetabotiuk@gmail.com>
2023-08-24 09:58:33 +0300

    changes in reports controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,11 +2,14 @@ class ReportsController < ApplicationController
   def index
   end
 
-  def report_by_category
-    @operation = Operation.where(category_id = ?, params[:category_id])
-  end
+  # def report_by_category
+  #   @operation = Operation.where(category_id = ?, params[:category_id])
+  # end
 
   def report_by_dates
-    @operation = Operation.where(created_at >= start_date, created_at <= end_date, params[:created_at])
+    rates_data = Operation.where(created_at >= start_date, created_at <= end_date, params[:created_at]).map { |o| [o.odate.to_s, o.amount] }
+    @dates = rates_data.map { |e| e[0] }
+    @rates = rates_data.map { |e| e[1] }
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'reports/index', to: 'reports#index'
+  get 'reports', to: 'reports#index'
   get 'reports/report_by_category', to: 'reports#report_by_category'
   get 'reports/report_by_dates', to: 'reports#report_by_dates'
   get 'main/index', to: 'main#index'
