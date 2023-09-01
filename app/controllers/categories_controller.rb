65925dfd91297c2811408f0baacadbc425c6f3ce
botiuk <svetabotiuk@gmail.com>
2023-09-01 04:19:44 +0300

    fixed grammar mistakes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,7 +25,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to category_url(@category), notice: "Категорія успішно створена." }
+        format.html { redirect_to category_url(@category), notice: "Категорія успішно створена" }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to category_url(@category), notice: "Категорія успішно оновлена." }
+        format.html { redirect_to category_url(@category), notice: "Категорія успішно оновлена" }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,11 +52,11 @@ class CategoriesController < ApplicationController
     respond_to do |format|
       if Operation.where(category_id: @category.id).size == 0 
         @category.destroy
-        format.html { redirect_to categories_url, notice: "Категорія успішно видалена." }
+        format.html { redirect_to categories_url, notice: "Категорія успішно видалена" }
         format.json { head :no_content }
       else
         format.html { redirect_to category_url(@category), 
-          alert: "Неможливо видалити! Є операції з даною категорією.", 
+          alert: "Неможливо видалити! Є операції з даною категорією", 
           status: :unprocessable_entity }
         format.json { render json: @category.errors, status: :unprocessable_entity }
       end
