aa10574415992c25f25c074bcb2018c92ffcd2b6
botiuk <svetabotiuk@gmail.com>
2023-09-02 06:04:17 +0300

    wrote seeds faker for operation

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,26 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# require "faker"
+require "faker"
 
+(18..50).each do |id|
+  Operation.create(
+    id: id,
+    amount: Faker::Number.between(from: 1000, to: 15000),
+    odate: Faker::Date.between(from: "2022-11-20", to: "2023-03-05"),
+    description: Faker::TvShows::DrWho.quote,
+    category_id: Faker::Number.between(from: 1, to: 9),
+    otype: 1,
+  )
+end
 
-# (18..50).each do |id|
-#   Operation.create(
-#     id: id,
-#     amount: Faker::Number.between(from: 1000, to: 50000),
-#     odate: Faker::Date.between(from: "2022-11-20", to: "2023-03-05"),
-#     description: Faker::TvShows::Simpsons.quote,
-#     category_id: Faker::Number.between(from: 1, to: 9),
-#     otype: 1,
-#   )
-# end
-
-# (51..500).each do |id|
-#   Operation.create(
-#     id: id,
-#     amount: Faker::Number.between(from: 0.1, to: 3000.0),
-#     odate: Faker::Date.between(from: "2022-11-20", to: "2023-03-05"),
-#     description: Faker::TvShows::Friends.quote,
-#     category_id: Faker::Number.between(from: 10, to: 40),
-#     otype: 0,
-#   )
-# end
+(51..500).each do |id|
+  Operation.create(
+    id: id,
+    amount: Faker::Number.decimal(l_digits: 3, r_digits: 1),
+    odate: Faker::Date.between(from: "2022-11-20", to: "2023-03-05"),
+    description: Faker::TvShows::Simpsons.quote,
+    category_id: Faker::Number.between(from: 10, to: 40),
+    otype: 0,
+  )
+end
