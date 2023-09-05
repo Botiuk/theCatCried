1b4eda2fe86b59d27b0935c282d3650cedc3e182
botiuk <svetabotiuk@gmail.com>
2023-09-05 06:31:24 +0300

    removed comments

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,74 +1,60 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: %i[ show edit update destroy ]
 
-  # GET /categories or /categories.json
   def index
     @categories = Category.list_order.page(params[:page])
   end
 
-  # GET /categories/1 or /categories/1.json
   def show
   end
 
-  # GET /categories/new
   def new
     @category = Category.new(:ctype => params[:ctype])
   end
 
-  # GET /categories/1/edit
   def edit
   end
 
-  # POST /categories or /categories.json
   def create
     @category = Category.new(category_params)
 
     respond_to do |format|
       if @category.save
         format.html { redirect_to category_url(@category), notice: "Категорія успішно створена" }
-        format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /categories/1 or /categories/1.json
   def update
     respond_to do |format|
       if @category.update(category_params)
         format.html { redirect_to category_url(@category), notice: "Категорія успішно оновлена" }
-        format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /categories/1 or /categories/1.json
   def destroy
     respond_to do |format|
       if Operation.cat_destroy(@category.id) == 0 
         @category.destroy
         format.html { redirect_to categories_url, notice: "Категорія успішно видалена" }
-        format.json { head :no_content }
       else
         format.html { redirect_to category_url(@category), 
           alert: "Неможливо видалити! Є операції з даною категорією", status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+  
     def set_category
       @category = Category.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def category_params
       params.require(:category).permit(:name, :description, :ctype)
     end
--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,7 +1,6 @@
 class OperationsController < ApplicationController
   before_action :set_operation, only: %i[ show edit update destroy ]
 
-  # GET /operations or /operations.json
   def index
     @operations = Operation.list_order.page(params[:page])
     @categories = Category.search_formhelper
@@ -12,67 +11,55 @@ class OperationsController < ApplicationController
     @search_name = Category.name_from_id(params[:category_id])
   end
 
-  # GET /operations/1 or /operations/1.json
   def show
   end
 
-  # GET /operations/new
   def new
     @operation = Operation.new(:otype => params[:otype], :odate => Time.now.to_date)
     @categories = Category.ctype_formhelper(@operation)    
   end
 
-  # GET /operations/1/edit
   def edit
     @categories = Category.edit_formhelper(@operation)
   end
 
-  # POST /operations or /operations.json
   def create
     @operation = Operation.new(operation_params)
     @categories = Category.ctype_formhelper(@operation)
     
     respond_to do |format|
       if @operation.save
-        format.html { redirect_to operation_url(@operation), notice: "Операція успішно створена" }
-        format.json { render :show, status: :created, location: @operation }        
+        format.html { redirect_to operation_url(@operation), notice: "Операція успішно створена" }      
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @operation.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /operations/1 or /operations/1.json
   def update
       respond_to do |format|
       if @operation.update(operation_params)
         format.html { redirect_to operation_url(@operation), notice: "Операція успішно оновлена" }
-        format.json { render :show, status: :ok, location: @operation }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @operation.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /operations/1 or /operations/1.json
   def destroy
     @operation.destroy
 
     respond_to do |format|
       format.html { redirect_to operations_url, notice: "Операція успішно видалена" }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_operation
       @operation = Operation.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def operation_params
       params.require(:operation).permit(:amount, :odate, :description, :category_id, :otype)
     end
