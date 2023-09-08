5e335cc6f2bde320409b15b38c142fbed5c3cea5
botiuk <svetabotiuk@gmail.com>
2023-09-08 09:56:22 +0300

    added faker ENV production

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,4 +35,44 @@ when "development"
 
 when "production"
 
+  (1..10).each do |id|
+    Category.create(
+      id: id,
+      name: Faker::ElectricalComponents.unique.active,
+      description: Faker::Movies::StarWars.quote,
+      ctype: 1,
+    )
+  end
+
+  (11..30).each do |id|
+    Category.create(
+      id: id,
+      name: Faker::ElectricalComponents.unique.passive,
+      description: Faker::Movies::VForVendetta.quote,
+      ctype: 0,
+    )
+  end
+
+  (1..100).each do |id|
+    Operation.create(
+      id: id,
+      amount: Faker::Number.between(from: 1000, to: 15000),
+      odate: Faker::Date.between(from: "2022-11-20", to: "2023-03-12"),
+      description: Faker::TvShows::DrWho.quote,
+      category_id: Faker::Number.between(from: 1, to: 10),
+      otype: 1,
+    )
+  end
+
+  (101..500).each do |id|
+    Operation.create(
+      id: id,
+      amount: Faker::Number.decimal(l_digits: 3, r_digits: 1),
+      odate: Faker::Date.between(from: "2022-11-20", to: "2023-03-12"),
+      description: Faker::TvShows::Simpsons.quote,
+      category_id: Faker::Number.between(from: 11, to: 30),
+      otype: 0,
+    )
+  end
+
 end
