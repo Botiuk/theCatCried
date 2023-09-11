1eea915887be37596a6f1fadc9b6d1dedff76d40
botiuk <svetabotiuk@gmail.com>
2023-09-11 11:19:02 +0300

    added autoupdate for copyright years

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,7 @@ class MainController < ApplicationController
 
   def index
     $total_amount = Operation.total_amount
+    Date.today.year == 2023 ? $copyright_year = "2023" : $copyright_year = "2023-#{Date.today.year.to_s}"
   end
   
 end
