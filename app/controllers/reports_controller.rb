66fd3070fee3ce642986b8ac4b7f6a5d4a4fddb5
botiuk <svetabotiuk@gmail.com>
2023-09-14 12:32:57 +0300

    changed strftime to %e %b

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -17,7 +17,7 @@ class ReportsController < ApplicationController
   def report_by_dates
     report_data_view
     @graph_data = Operation.reports_data_by_dates(@start_date, @end_date, @otype, @category_id)
-    @dates = @graph_data.map { |e| e[0].to_date.strftime('%d.%m') }
+    @dates = @graph_data.map { |e| e[0].to_date.strftime('%e %b') }
     @moneys = @graph_data.map { |e| e[1] }    
     @period_sum = Operation.reports_sum(@start_date, @end_date, @otype, @category_id)
   end
