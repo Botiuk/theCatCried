a13ba8f3bbb29e6efcc9abcd46711cc6634be24f
botiuk <svetabotiuk@gmail.com>
2023-09-15 05:06:36 +0300

    added switching between locales to header

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  get 'reports', to: 'reports#index'
-  get 'reports/report_by_category', to: 'reports#report_by_category'
-  get 'reports/report_by_dates', to: 'reports#report_by_dates'
-  get '/search', to: 'operations#search'
-  get 'main/index', to: 'main#index'
-  resources :operations
-  resources :categories
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "main#index"
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    get 'reports', to: 'reports#index'
+    get 'reports/report_by_category', to: 'reports#report_by_category'
+    get 'reports/report_by_dates', to: 'reports#report_by_dates'
+    get '/search', to: 'operations#search'
+    get 'main/index', to: 'main#index'
+    resources :operations
+    resources :categories
+
+    root "main#index"
+  end
+  
 end
