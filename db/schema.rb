2d4dc790379361128a89a36427e1529616214fe6
botiuk <svetabotiuk@gmail.com>
2023-09-23 22:34:17 +0300

    added user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_11_054344) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_23_193011) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -28,6 +28,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_11_054344) do
     t.datetime "updated_at", null: false
     t.integer "otype"
     t.index ["category_id"], name: "index_operations_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "operations", "categories"
