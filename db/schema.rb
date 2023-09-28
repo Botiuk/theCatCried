48b1a1c72041b08903b27975ee92a65410563c54
botiuk <svetabotiuk@gmail.com>
2023-09-28 08:51:01 +0300

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_23_193011) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_27_093016) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "ctype"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "operations", force: :cascade do |t|
@@ -27,7 +29,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_193011) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "otype"
+    t.integer "user_id", null: false
     t.index ["category_id"], name: "index_operations_on_category_id"
+    t.index ["user_id"], name: "index_operations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,9 +42,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_193011) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
   add_foreign_key "operations", "categories"
+  add_foreign_key "operations", "users"
 end
