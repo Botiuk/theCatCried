9e00322ec1f2ca97da95c1e1d83ec625a39d1db8
botiuk <svetabotiuk@gmail.com>
2023-10-03 08:54:47 +0300

    changed column default to operation amount

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_27_093016) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_03_054552) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_093016) do
   end
 
   create_table "operations", force: :cascade do |t|
-    t.decimal "amount", precision: 10, scale: 2, default: "0.0"
+    t.decimal "amount", precision: 10, scale: 2
     t.datetime "odate"
     t.string "description"
     t.integer "category_id", null: false
