661d1ae705e97a82144568f328b90af311833172
botiuk <svetabotiuk@gmail.com>
2023-10-10 13:49:11 +0300

    added params user_id to reports controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -7,19 +7,19 @@ class ReportsController < ApplicationController
       
   def report_by_category
     report_data_view
-    @diagram_data = Operation.reports_data_by_category(@start_date, @end_date, @otype, @category_id).sort
+    @diagram_data = Operation.reports_data_by_category(@start_date, @end_date, @otype, @category_id, @user_id).sort
     @cat_name= @diagram_data.map { |e| e[0] }
     @cat_moneys = @diagram_data.map { |e| e[1] }    
-    @period_sum = Operation.reports_sum(@start_date, @end_date, @otype, @category_id)
+    @period_sum = Operation.reports_sum(@start_date, @end_date, @otype, @category_id, @user_id)
     @colors = (@diagram_data.size).times.map {"#" + "%06x" % (rand * 0xffffff)}
   end
 
   def report_by_dates
     report_data_view
-    @graph_data = Operation.reports_data_by_dates(@start_date, @end_date, @otype, @category_id)
+    @graph_data = Operation.reports_data_by_dates(@start_date, @end_date, @otype, @category_id, @user_id)
     @dates = @graph_data.map { |e| I18n.l(e[0].to_date, format: :short) }
     @moneys = @graph_data.map { |e| e[1] }    
-    @period_sum = Operation.reports_sum(@start_date, @end_date, @otype, @category_id)
+    @period_sum = Operation.reports_sum(@start_date, @end_date, @otype, @category_id, @user_id)
   end
 
   private
@@ -37,6 +37,8 @@ class ReportsController < ApplicationController
     if @category_id != "0"
       @category_name = Category.name_from_id(@category_id)
     end  
+
+    @user_id = params[:user_id]
   end
 
 end
