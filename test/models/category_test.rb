fb2fce4712032fc258cfb8a1a32121fb88fab491
botiuk <svetabotiuk@gmail.com>
2023-10-12 20:45:29 +0300

    changed tests for model Category

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -8,14 +8,14 @@ class CategoryTest < ActiveSupport::TestCase
   end
 
   test "saving and gathering" do
-    new_category = Category.new(name: "new category", description: "new description", ctype: 0)
+    new_category = Category.new(name: "new category", description: "new description", ctype: 0, user_id: User.find_by(username: 'First').id)
     new_category.save()
     new_cat = Category.find_by(name: "new category")
     assert_equal("new description", new_cat.description)
   end
 
   test "saving and updating" do
-    new_category = Category.new(name: "new category", description: "new description", ctype: 0)
+    new_category = Category.new(name: "new category", description: "new description", ctype: 0, user_id: User.find_by(username: 'First').id)
     new_category.save()
     new_category.update(name: "update category")
     update_category = Category.find_by(name: "update category")
@@ -23,7 +23,7 @@ class CategoryTest < ActiveSupport::TestCase
   end
 
   test "saving and destroying" do
-    new_category = Category.new(name: "destroy category", description: "new description", ctype: 0)
+    new_category = Category.new(name: "destroy category", description: "new description", ctype: 0, user_id: User.find_by(username: 'First').id)
     new_category.save()
     new_category.destroy()
     destroy_category = Category.find_by(name: "destroy category")
@@ -33,36 +33,36 @@ class CategoryTest < ActiveSupport::TestCase
 #testing creating new category
 
   test "return false if name is missed" do
-    new_category = Category.new(description: "some description", ctype: 0)
+    new_category = Category.new(description: "some description", ctype: 0, user_id: User.find_by(username: 'First').id)
     assert_not(new_category.valid?)
   end
 
   test "return false if description is missed" do
-    new_category = Category.new(name: "some name", ctype: 0)
+    new_category = Category.new(name: "some name", ctype: 0, user_id: User.find_by(username: 'First').id)
     assert_not(new_category.valid?)
   end
 
   test "return false if ctype is missed" do
-    new_category = Category.new(name: "some name", description: "other description")
+    new_category = Category.new(name: "some name", description: "other description", user_id: User.find_by(username: 'First').id)
     assert_not(new_category.valid?)
   end
 
   test "return false if name not unique" do
-    new_category1 = Category.new(name: "some name", description: "some description", ctype: 0)
+    new_category1 = Category.new(name: "some name", description: "some description", ctype: 0, user_id: User.find_by(username: 'First').id)
     new_category1.save()
-    new_category2 = Category.new(name: "some name", description: "other description", ctype: 0)
+    new_category2 = Category.new(name: "some name", description: "other description", ctype: 0, user_id: User.find_by(username: 'First').id)
     assert_not(new_category2.valid?)
   end
 
   test "return true if description not unique" do
-    new_category1 = Category.new(name: "some name", description: "some description", ctype: 0)
+    new_category1 = Category.new(name: "some name", description: "some description", ctype: 0, user_id: User.find_by(username: 'First').id)
     new_category1.save()
-    new_category2 = Category.new(name: "other name", description: "some description", ctype: 0)
+    new_category2 = Category.new(name: "other name", description: "some description", ctype: 0, user_id: User.find_by(username: 'First').id)
     assert(new_category2.valid?)
   end
 
   test "return true if everything is good" do
-    new_category = Category.new(name: "new category", description: "new description", ctype: 1)
+    new_category = Category.new(name: "new category", description: "new description", ctype: 1, user_id: User.find_by(username: 'Second').id)
     assert(new_category.valid?) 
   end
   
