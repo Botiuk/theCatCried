d2edcf289ddad460af0100b9d13cc823d00bd9e6
botiuk <svetabotiuk@gmail.com>
2023-10-19 12:58:28 +0300

    changed description in operation to can be blank

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -6,7 +6,6 @@ class Operation < ApplicationRecord
 
   validates :amount, presence: true, numericality: { greater_than: 0 }
   validates :odate, presence: true
-  validates :description, presence: true
   validates :otype, presence: true
 
   enum :otype, { outcome: 0, income: 1 }, prefix: true
