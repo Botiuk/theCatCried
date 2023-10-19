14d19dce9a116296fa3ccf96715e4fb6a4046237
botiuk <svetabotiuk@gmail.com>
2023-10-19 21:21:08 +0300

    added validates uniq to category name for user_id

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,7 @@ class Category < ApplicationRecord
   has_many :operations
   belongs_to :user
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { scope: :user_id }
   validates :description, presence: true
   validates :ctype, presence: true
 
