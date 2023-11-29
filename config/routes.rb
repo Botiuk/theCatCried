76f9106f1a4f92eb4ab5a3f475a4230726a4dfa4
botiuk <svetabotiuk@gmail.com>
2023-11-29 11:31:56 +0200

    changed operations search route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     get 'reports', to: 'reports#index'
     get 'reports/report_by_category', to: 'reports#report_by_category'
     get 'reports/report_by_dates', to: 'reports#report_by_dates'
-    get '/search', to: 'operations#search'
+    get 'operations/search', to: 'operations#search'
     get 'main/index', to: 'main#index'
     get 'about', to: 'about#index'
     resources :operations
