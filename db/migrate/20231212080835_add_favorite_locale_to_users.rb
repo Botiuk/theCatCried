3b42cf480bdf753dcf4649cee8225f539d3ded8c
botiuk <svetabotiuk@gmail.com>
2023-12-12 11:29:19 +0200

    add locale to User

---
+++ db/migrate/20231212080835_add_favorite_locale_to_users.rb
@@ -0,0 +1,5 @@
+class AddFavoriteLocaleToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :favorite_locale, :string, null: false, default: "en"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_03_054552) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_12_080835) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_03_054552) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username", default: "", null: false
+    t.string "favorite_locale", default: "en", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
