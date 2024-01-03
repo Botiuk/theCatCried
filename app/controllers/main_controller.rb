6eeaabdf740901f31890475157f99a8b7a3cf8ca
botiuk <svetabotiuk@gmail.com>
2024-01-03 13:31:07 +0200

    simplify copyright year

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,6 @@ class MainController < ApplicationController
 
   def index
     $total_amount = Operation.total_amount(current_user.id) if user_signed_in?
-    Date.today.year == 2023 ? $copyright_year = "2023" : $copyright_year = "2023-#{Date.today.year.to_s}"
   end
 
 end
